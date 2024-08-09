dc68182622aee191aef762c8901c35a617dac367
Mwamtindi <mwamtindishaban@gmail.com>
2024-08-09 18:45:50 +0300

    keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -14,19 +14,33 @@ def do_clean(number=0):
     Function that Delete the out-of-date archives.
     Args:
         number (int): The number of archives to keep.
+    Returns:
+        int: The number of archives cleaned remotely.
     """
     number = 1 if int(number) == 0 else int(number)
 
     # Clean local archives
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(min(number, len(archives)))]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    try:
+        archives = sorted(os.listdir("versions"))
+        [archives.pop() for i in range(min(number, len(archives)))]
+        with lcd("versions"):
+            [local("rm ./{}".format(a)) for a in archives]
+    except Exception:
+        pass
 
     # Clean remote archives
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
-        archives.reverse()  # Newest first
-        [archives.pop() for i in range(min(number, len(archives)))]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        archives.sort(reverse=True)  # Newest first
+        to_keep = archives[:number]
+        to_delete = archives[number:]
+        if to_delete:
+            run("rm -rf {}".format(" ".join(to_delete)))
+
+        return len(to_delete)
+
+
+if __name__ == "__main__":
+    result = do_clean()
+    print(result)
